f046be71c9f933c309d35f542b36bc5b413a54d3
Pablo Occhiuzzi <o.pablon@gmail.com>
2022-09-04 20:01:35 -0300

    app.use(express.static())

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require("express");
 let app = express();
 
-console.log("Hello World");
+app.use(express.static(__dirname + "/public/"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 module.exports = app;
