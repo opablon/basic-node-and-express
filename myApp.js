97a405bd3e752ab3b6e19f706c1d34d4b45fe406
Pablo Occhiuzzi <104530403+opablon@users.noreply.github.com>
2022-10-19 17:37:24 -0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,23 +1,47 @@
-let express = require("express");
+let express = require('express');
 let app = express();
-require("dotenv").config();
+let bodyParser = require('body-parser')
+
+app.use(function simpleLogger(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// app.get("/json", function (req, res) {
-//   res.json({ message: "Hello json" });
-// });
-
-app.get("/json", function (req, res) {
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    res.json({ message: "HELLO JSON" });
+app.get("/json", (req, res) => {
+  if (process.env['MESSAGE_STYLE'] === "uppercase") {
+    res.json({ message: "HELLO JSON" });;
   } else {
     res.json({ message: "Hello json" });
-  }
+  };
 });
 
-module.exports = app;
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({time: req.time});
+});
+
+app.get("/:word/echo", (req, res) => {
+  res.json({echo: req.params.word});
+});
+
+app.get("/name", (req, res) => {
+  let { first: firstName, last: lastName } = req.query;
+  res.json({name: `${firstName} ${lastName}`});
+});
+
+app.post('/name', (req, res) => {
+  let { first: firstName, last: lastName } = req.body;
+  res.json({name: `${firstName} ${lastName}`});
+});
+
+ module.exports = app;
